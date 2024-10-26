e56e363cda7a512556ecaf95d28a5f3ada3ce6c5
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-10-26 18:08:40 -0300

    add: nuevas rutas del back y metabase

--- src/connection/backUrl.js
+++ src/connection/backUrl.js
@@ -1,6 +1,8 @@
 //export const backendUrl = import.meta.env.VITE_API_URL
 
-//export const backendUrl = "http://18.209.5.94:8080"
+//export const backendUrl = " http://18.208.162.228:8080" //NUEVA
 export const backendUrl = "http://localhost:8080"
 
 export const metaBaseUrl = "http://54.175.63.39:3000"
+//export const metaBaseUrl = "http://3.91.85.65:3000" //NUEVA
+
