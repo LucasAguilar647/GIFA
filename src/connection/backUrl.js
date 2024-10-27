466db32803eab77c1727b59dd71904cde8792325
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-10-27 15:54:36 -0300

    fix: puse las nuevas rutas

--- src/connection/backUrl.js
+++ src/connection/backUrl.js
@@ -1,8 +1,8 @@
 //export const backendUrl = import.meta.env.VITE_API_URL
 
-//export const backendUrl = " http://18.208.162.228:8080" //NUEVA
+//export const backendUrl = "http://18.208.162.228:8080" //NUEVA
 export const backendUrl = "http://localhost:8080"
 
 export const metaBaseUrl = "http://54.175.63.39:3000"
-//export const metaBaseUrl = "http://3.91.85.65:3000" //NUEVA
+//export const metaBaseUrl = "http://98.81.10.29:3000" //NUEVA
 
