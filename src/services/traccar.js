960fcd76a17b863b4d24f1efe4f06913ade866f3
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-11-01 11:57:58 -0300

    add: ruta para obtener los dispositivos

--- src/services/traccar.js
+++ src/services/traccar.js
@@ -6,3 +6,8 @@ export const crearDispositivo = async (data, token) => {
     const endpoint = backendUrl + '/traccar/crearDispositivo';
     return await executeFetch(endpoint, data, HttpMethods.POST, token, 201);
 };
+
+export const verDispositivos = async (token) => {
+    const endpoint = backendUrl + '/traccar/getDispositivos';
+    return await executeFetch(endpoint, null, HttpMethods.GET, token, 200);
+};
