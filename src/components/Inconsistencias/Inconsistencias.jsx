8a1b197d9e35dbfa7d160c17f361ae707d8b159d
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-11-02 21:12:33 -0300

    add: componente aopra cargar inconsistencais

--- src/components/Inconsistencias/Inconsistencias.jsx
+++ src/components/Inconsistencias/Inconsistencias.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { verInconsistencias } from '../../services/traccar';
+import { useSelector } from 'react-redux';
 
 export const Inconsistencias = () => {
-
-
+    const token = useSelector((state) => state.user.token);
 
     
+    const data = {
+        fecha: new Date().toISOString() 
+    };
 
+    const fetchData = async () => {
+        try {
+            const response = await verInconsistencias(data, token);
+            console.log(response); 
+        } catch (error) {
+            console.error("Error fetching data: ", error);
+        } 
+    };
 
+    
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-  return (
-    <div>Inconsistencias</div>
-  )
-}
+    return (
+        <div>Inconsistencias</div>
+    );
+};
--- src/services/traccar.js
+++ src/services/traccar.js
@@ -11,3 +11,8 @@ export const verDispositivos = async (token) => {
     const endpoint = backendUrl + '/traccar/getDispositivos';
     return await executeFetch(endpoint, null, HttpMethods.GET, token, 200);
 };
+
+export const verInconsistencias = async (data,token) => {
+    const endpoint = backendUrl + '/traccar/verInconsistenciasDeCombustible';
+    return await executeFetch(endpoint, data, HttpMethods.GET, token, 200);
+};
