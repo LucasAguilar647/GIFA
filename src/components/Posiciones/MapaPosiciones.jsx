d79bcbcfb55fe72516555acbfe643bd37914090c
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-11-06 17:52:41 -0300

    add: se muestran los puntos en el mapa

--- src/components/Posiciones/MapaPosiciones.jsx
+++ src/components/Posiciones/MapaPosiciones.jsx
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Polyline } from 'react-leaflet';
+import { MapContainer, TileLayer, Polyline, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+
 
 const MapaPosiciones = ({ posiciones }) => {
   const [center, setCenter] = useState([0, 0]);
 
   useEffect(() => {
     if (posiciones.length > 0) {
-      setCenter(posiciones[0]);
+      setCenter(posiciones[0]); 
     }
   }, [posiciones]);
 
-  // Si no hay posiciones, muestra un mensaje
   if (posiciones.length === 0) {
     return <p>El colectivo no tiene recorridos hechos.</p>;
   }
@@ -22,7 +22,18 @@ const MapaPosiciones = ({ posiciones }) => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; <a href='https://osm.org/copyright'>OpenStreetMap</a> contributors"
       />
+
+      
       {posiciones.length > 1 && <Polyline positions={posiciones} color="red" />}
+
+    
+      {posiciones.map((pos, index) => (
+        <Marker key={index} position={pos}>
+          <Popup>
+            <div>Posición: {pos}</div>
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 };
