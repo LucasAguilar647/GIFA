2c98dd9993987f78ef834f5db0c721751dc41c30
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-11-09 15:30:56 -0300

    add: cambie sl css de posiciones

--- src/components/Posiciones/Posiciones.jsx
+++ src/components/Posiciones/Posiciones.jsx
@@ -2,42 +2,49 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { verPosiciones } from '../../services/traccar';
 import MapaPosiciones from './MapaPosiciones';
-import './styles/Posiciones.css'
+import './styles/Posiciones.css';
 import { Button } from '@nextui-org/react';
 
 export const Posiciones = () => {
   const token = useSelector((state) => state.user.token);
   const [id, setId] = useState("");
   const [positions, setPositions] = useState([]); 
+  const [bucarClickeado, setBuscarClickeado] = useState(false); 
 
-  
   const fetchPosiciones = async () => {
-    try {
-      const response = await verPosiciones(id, token);
-      const posiciones = response.map((pos) => [pos.latitude, pos.longitude]);
-      setPositions(posiciones);
-    } catch (error) {
-      console.error("Error fetching positions:", error);
-      setPositions([]); 
+    if (id.trim() !== "") {
+      try {
+        setBuscarClickeado(true); 
+        const response = await verPosiciones(id, token);
+        const posiciones = response.map((pos) => [pos.latitude, pos.longitude]);
+        setPositions(posiciones);
+      } catch (error) {
+        console.error("Error fetching positions:", error);
+        setPositions([]); 
+      }
+    } else {
+      alert("Por favor, ingrese una patente.");
     }
   };
 
   return (
     <div>
-      <h2>Recorrido de colectivo</h2>
+      <h1>Recorrido de colectivo</h1>
       <input
         type="text"
         value={id}
         onChange={(e) => setId(e.target.value.toUpperCase())}
         placeholder="Ingrese la patente del colectivo"
       />
-      <Button color='primary' onClick={fetchPosiciones}>Buscar</Button>
+      <Button color="primary" onClick={fetchPosiciones}>Buscar</Button>
 
-     
+
       {positions.length > 0 ? (
         <MapaPosiciones posiciones={positions} />
       ) : (
-        <p>El colectivo no tiene recorridos hechos.</p>
+        bucarClickeado && positions.length === 0 && (
+          <p>El colectivo no tiene recorridos hechos.</p>
+        )
       )}
     </div>
   );
