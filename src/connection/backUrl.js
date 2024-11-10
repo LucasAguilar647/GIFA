07775c0484587bd4768ee5b03c1a8f577d905ad6
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-11-10 10:31:54 -0300

    Produccion

--- src/connection/backUrl.js
+++ src/connection/backUrl.js
@@ -1,7 +1,7 @@
 //export const backendUrl = import.meta.env.VITE_API_URL
 
-//export const backendUrl = "http://18.208.162.228:8080" //NUEVA
-export const backendUrl = "http://localhost:8080"
+export const backendUrl = "http://18.208.162.228:8080" //NUEVA
+//export const backendUrl = "http://localhost:8080"
 
 
 export const metaBaseUrl = "http://98.81.10.29:3000" //NUEVA
