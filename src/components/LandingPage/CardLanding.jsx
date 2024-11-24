d9fe7b6b67d924b1ed5832b0545680eb5c97fb95
Lucas_Aguilar <133165579+LucasAguilar647@users.noreply.github.com>
2024-11-24 18:34:46 -0300

    add: video de presentacion al principio

--- src/components/LandingPage/CardLanding.jsx
+++ src/components/LandingPage/CardLanding.jsx
@@ -1,22 +1,24 @@
-import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
+import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+import './CardLanding.css';
 
-export const CardLanding=({titulo,imagen})=> {
+export const CardLanding = ({ titulo, imagen, desc }) => {
   return (
-    <Card className="py-4">
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-      <h4 className="font-bold text-large">{titulo}</h4>
-     
+    <Card className="card-landing">
+      <CardHeader className="card-header">
+        <h4 className="card-title">{titulo}</h4>
       </CardHeader>
-      <CardBody className="overflow-visible py-2">
+      <CardBody className="card-body">
         <Image
-          alt="Card background"
-          className="object-cover rounded-xl"
+          alt="Imagen descriptiva"
+          className="card-image"
           src={imagen}
-          width={270}
+          width={90}
+          height={90}
         />
+        <p className="card-desc">{desc}</p>
       </CardBody>
     </Card>
   );
-}
+};
 
 export default CardLanding;
