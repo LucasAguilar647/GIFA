deddcfc82d597fc9c737a02bce91255b23d1481f
Tomas Ledesma <tomasld13@gmail.com>
2024-11-28 23:41:00 -0300

    fix: update ip back

--- src/connection/backUrl.js
+++ src/connection/backUrl.js
@@ -1,6 +1,6 @@
 //export const backendUrl = import.meta.env.VITE_API_URL
 
-export const backendUrl = "http://18.208.162.228:8080" //NUEVAA
+export const backendUrl = "https://gifaapplication.gifa.life" //NUEVAA
 //export const backendUrl = "http://localhost:8080"
 
 
