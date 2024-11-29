59d6a6a2aa86d140db3637c40a082924f10d7c55
Tomas Ledesma <tomasld13@gmail.com>
2024-11-29 13:28:30 -0300

    fix: metaBaseUrl

--- src/connection/backUrl.js
+++ src/connection/backUrl.js
@@ -4,5 +4,5 @@ export const backendUrl = "https://gifaapplication.gifa.life" //NUEVAA
 //export const backendUrl = "http://localhost:8080"
 
 
-export const metaBaseUrl = "http://54.166.240.179:3000" //NUEVA
+export const metaBaseUrl = "https://gifametabase.gifa.life/" //NUEVA
 
